1a80062b37a5ca49f0d48611eecb10888fc670af
Oleg Grenrus <oleg.grenrus@iki.fi>
2016-12-13 20:54:28 +0200

    Allow empty connectHost

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# 0.2.0.0
+
+Allow empty host, i.e. don't use default provided by `postgresql-simple`.
+Empty host is required to use unix domain sockets.
--- tests/Tests.hs
+++ tests/Tests.hs
@@ -9,9 +9,15 @@ main = defaultMain casesProps
 
 cases :: [(String, Maybe ConnectInfo)]
 cases =
-  [ ("postgres:///local", Just $ defaultConnectInfo { connectDatabase = "local"} )
+  [ ("postgres:///local", Just $ defaultConnectInfo
+    { connectHost = "", connectDatabase = "local"} )
+  , ("postgres://localhost/local", Just $ defaultConnectInfo
+    { connectHost = "localhost", connectDatabase = "local"} )
+  , ("postgres://user@/local", Just $ defaultConnectInfo
+    { connectHost = "", connectUser = "user", connectDatabase = "local"} )
   , ("mysql:///typo", Nothing)
-  , ("postgres://foo:bar@example.com:2345/database", Just $ ConnectInfo "example.com" 2345 "foo" "bar" "database")
+  , ("postgres://foo:bar@example.com:2345/database", Just $
+    ConnectInfo "example.com" 2345 "foo" "bar" "database")
   ]
 
 casesProps :: TestTree
